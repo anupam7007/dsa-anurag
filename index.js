5025d6b865e0ae5b1920d43b1f7f87c2c36c164d
anupam7007 <57726905+anupam7007@users.noreply.github.com>
2021-07-30 02:22:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById("sortingVis").addEventListener("click", function(){
+    window.location.pathname = "/sorting visualizer/index.html"; 
+})
+
+document.getElementById("splitwise").addEventListener("click", function(){
+    window.location.pathname = "/splitwise/splitwise.html"; 
+})
+
+document.getElementById("travelPlanner").addEventListener("click", function(){
+    window.location.pathname = "/travel planner/index.html"; 
+})
+
+
+
+    
+
